34161742248af4776cc810714944532ed8115d18
John Siwicki <johnsiwicki@Johns-iMac.local>
2015-10-08 11:15:24 -0400

    CSS updates and content updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 $(document).foundation();
 
+if (document.location.pathname === "/") {
+   // do some special stuff on this page!
+   $( document ).ready(function() {
+   	//disable scrolling on loading 
+   	$("body").css("overflow", "hidden");
+
+	$( ".animsition-link" ).click(function(event) {
+			 event.preventDefault();
+		 	 $(this).velocity("transition.fadeOut", 250);
+		 	 $('.fullscreen_show').velocity({ height: 0 }, 1250);
+		 	 //reset to 
+		 	 $("body").css("overflow", "auto");
+		});
+
+});
+
+}
+
+
 $(window).scroll(function(){
  	var length = '300';
  	$(".hotel-left").stop().animate({"right": (length) + "px"}, 600);
   	$(".hotel-right").stop().animate({"left": (length) + "px"}, 600 );
-});
-
- 
-$( document ).ready(function() {
-	$( ".animsition-link" ).click(function(event) {
-	 event.preventDefault();
- 	 $(this).velocity("transition.fadeOut", 250);
- 	 $('.fullscreen_show').velocity({ height: 0 }, 1250);
- 	 //$(".fullscreen_show").stop().velocity({"height": (length) + "px"}, 2000 );
- 	 //$(".fullscreen_show").delay(1000).velocity("slideUp", 950);
-});
-
-
-
 });
